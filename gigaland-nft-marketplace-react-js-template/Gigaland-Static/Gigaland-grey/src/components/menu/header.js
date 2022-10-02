f2ce1aca10cb96e06bc7c238f5276efb4409c0c5
RITEKROUNAK <ritekrounak1@gmail.com>
2022-10-02 08:15:36 +0530

    Link Updated

--- gigaland-nft-marketplace-react-js-template/Gigaland-Static/Gigaland-grey/src/components/menu/header.js
+++ gigaland-nft-marketplace-react-js-template/Gigaland-Static/Gigaland-grey/src/components/menu/header.js
@@ -142,7 +142,7 @@ const Header= function() {
               </BreakpointProvider>
 
               <div className='mainside'>
-                <NavLink to="https://main-page-hack.vercel.app/" className="btn-main">Join Us</NavLink>
+                <NavLink to={{pathname: "https://main-page-hack.vercel.app/"}} className="btn-main">Join Us</NavLink>
               </div>
                   
       </div>
